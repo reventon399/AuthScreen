48ce2858a5c38839e56006997ae42e5002e033f1
Alexey <alexeylosev1501@gmail.com>
2022-08-18 21:18:02 +0300

    Add Outlets.

--- AuthScreen/ViewController.swift
+++ AuthScreen/ViewController.swift
@@ -10,6 +10,86 @@ import SnapKit
 
 class ViewController: UIViewController {
 
+    //MARK: - Outlets
+    
+    private let backgroundView: UIImageView = {
+        let imageView = UIImageView()
+        
+        return imageView
+    }()
+    
+    private let loginLabel: UILabel = {
+        let label = UILabel()
+        
+        return label
+    }()
+    
+    private let loginTextField: UITextField = {
+        let textField = UITextField()
+        
+        return textField
+    }()
+    
+    private let passwordTextField: UITextField = {
+       let textField = UITextField()
+        
+        return textField
+    }()
+    
+    private let loginButton: UIButton = {
+       let button = UIButton()
+        
+        return button
+    }()
+    
+    private let forgotYourPasswordButton: UIButton = {
+        let button = UIButton()
+        
+        return button
+    }()
+    
+    private let leftFromOrConnectWithLabelView: UIView = {
+       let view = UIView()
+        
+        return view
+    }()
+    
+    private let orConnectWithLabel: UILabel = {
+       let label = UILabel()
+        
+        return label
+    }()
+    
+    private let rightFromOrConnectWithLabelView: UIView = {
+       let view = UIView()
+        
+        return view
+    }()
+    
+    private let facebookButton: UIButton = {
+        let button = UIButton()
+        
+        return button
+    }()
+    
+    private let twitterButton: UIButton = {
+        let button = UIButton()
+        
+        return button
+    }()
+    
+    private let dontHaveAnAccountLabel: UILabel = {
+       let label = UILabel()
+        
+        return label
+    }()
+    
+    private let signUpButton: UIButton = {
+       let button = UIButton()
+        
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
